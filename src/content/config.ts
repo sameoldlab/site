5db0cd98cc3496c0890c624f566552466a06ba3f
ibro <sameoldlab@proton.me>
2024-06-21 19:24:02 +0100

    new collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,45 @@
-import { z, defineCollection } from "astro:content"
+import { z, defineCollection } from 'astro:content'
 
 // src: https://github.com/chrismwilliams/astro-theme-cactus/blob/main/src/content/config.ts
 function removeDupsAndLowerCase(array: string[]) {
-  if (array.length === 0) return array
-  return Array.from(new Set(array.map((str) => str.toLowerCase())))
+	if (array.length === 0) return array
+	return Array.from(new Set(array.map(str => str.toLowerCase())))
 }
 
 const note = defineCollection({
-  schema: z.object({
-    title: z.string().optional().default(''),
-    date: z.date(),
-    tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
-    draft: z.boolean().optional(),
-  }),
+	schema: z.object({
+		title: z.string().optional().default(''),
+		date: z.date(),
+		tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
+		draft: z.boolean().optional(),
+	}),
 })
 
-export const collections = { note }
+const stream = defineCollection({
+	schema: z.object({
+		date: z.date(),
+		tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
+	}),
+})
+
+const project = defineCollection({
+	type: 'data',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		links: z.array(z.object({}).catchall(z.string().url())).optional(),
+		media: z
+			.array(
+				z.object({
+					link: z.string(),
+					// type: z.enum([z.literal('image'), z.literal('video')]),
+					"type": z.enum(["image", "video"]),
+					alt: z.string(),
+					description: z.string().optional(),
+				})
+			)
+			.optional(),
+	}),
+})
+
+export const collections = { note, stream, project }
