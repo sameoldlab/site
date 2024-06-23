a5f28f64444877d23f0c45f918503dd94b6aa491
ibro <sameoldlab@proton.me>
2024-06-23 17:14:51 +0100

    speed insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ export default defineConfig({
 		},
 		imageService: true,
 		devImageService: 'sharp',
-		webAnalytics: true,
+		webAnalytics: {enabled: true},
 	}),
 	image: {
 		domains: ['ibro.xyz'],
