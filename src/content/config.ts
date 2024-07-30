0b1035aa240734e78faf922c0c183b7649b179e7
ibro <sameoldlab@proton.me>
2024-07-30 03:07:39 +0100

    formatting

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { z, defineCollection } from 'astro:content'
 // src: https://github.com/chrismwilliams/astro-theme-cactus/blob/main/src/content/config.ts
 function removeDupsAndLowerCase(array: string[]) {
 	if (array.length === 0) return array
-	return Array.from(new Set(array.map(str => str.toLowerCase())))
+	return Array.from(new Set(array.map((str) => str.toLowerCase())))
 }
 
 const note = defineCollection({
