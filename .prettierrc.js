0535db582c119955ab17e6a3cd2f72b408b7d2c8
ibro <sameoldlab@proton.me>
2024-07-30 03:00:24 +0100

    setup prettier

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+const config = {
+	plugins: ['prettier-plugin-astro'],
+	trailingComma: 'es5',
+	quoteProps: 'consistent',
+	semi: false,
+	singleQuote: true,
+	useTabs: true,
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+}
+
+export default config
