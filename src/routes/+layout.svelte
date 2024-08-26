59b11cb7ede2096c27c9758fe7a1cf490a2190bf
ibro <sameoldlab@proton.me>
2024-08-26 19:26:43 +0100

    other

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,36 @@
 <script lang="ts">
 	import '$lib/css/main.css'
+	import '$lib/css/type.css'
 	import Nav from '$lib/components/nav.svelte'
 	import Meta from '$lib/components/meta.svelte'
+	import { page } from '$app/stores'
 	import type { MetaProps } from '$lib/types'
 	import type { Snippet } from 'svelte'
+	import { fade, fly, slide } from 'svelte/transition'
 
 	interface Props extends MetaProps {
 		children: Snippet
 	}
 	const { children, ...props }: Props = $props()
+	console.log($page.data)
+	const meta: MetaProps = {
+		title: $page.data?.title ?? 'ibro.xyz',
+		description:
+			$page.data?.description ??
+			'Experiments, exploration, bean-feuled execution',
+		type: 'website',
+		image: {}
+	}
 </script>
 
 <svelte:head>
-	<title>{props.title}</title>
-	<Meta {...props} />
+	<title>{meta.title}</title>
+	<Meta {...meta} />
 </svelte:head>
 
+{#key $page.route.id}
+	<div in:fade={{ duration: 260 }}>
+		{@render children()}
+	</div>
+{/key}
 <Nav />
-{@render children()}
--- src/routes/now/+page.svelte
+++ src/routes/now/+page.svelte
@@ -1,18 +1,19 @@
----
-import Layout from '../_inc/layouts/base.astro'
----
+<script lang="ts">
+	import Layout from '$lib/layouts/base.svelte'
+</script>
 
-<Layout title='ibro.xyz'>
-	<div id='p404'>
+<Layout title="ibro.xyz">
+	<div id="p404">
 		<h1>Now</h1>
 		<br />
 
 		<p class="pt-16">
-			I thought this would look mindful and reflective. (It's also easier to track for things
-			still in progress).
+			I thought this would look mindful and reflective. (It's also easier to
+			track for things still in progress).
 		</p>
 	</div>
 </Layout>
+
 <style>
 	div#p404 {
 		padding-block-start: 8rem;
