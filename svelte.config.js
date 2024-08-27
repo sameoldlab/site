3b946062f71a465862bd637ff6f1c0e43edae891
ibro <sameoldlab@proton.me>
2024-08-27 16:06:54 +0100

    handleHttpError

--- svelte.config.js
+++ svelte.config.js
@@ -11,12 +11,19 @@ const config = {
 		markdownPreprocess(),
 		sveltePreprocess()
 	],
-
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				if (['/note/nft-folder', '/note/visor', '/note/fractl-ui'].includes(path))
+					return
+
+				console.error(`no page found for ${path} from ${referrer}\n${message}`)
+			}
+		}
 	}
 };
 
