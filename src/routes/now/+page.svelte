c8613524e847447f98e24c936eccaf71ac4afab9
ibro <sameoldlab@proton.me>
2024-08-27 19:17:02 +0100

    now

--- src/routes/now/+page.svelte
+++ src/routes/now/+page.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
-	import Layout from '$lib/layouts/base.svelte'
 </script>
 
-<Layout title="ibro.xyz">
-	<div id="p404">
-		<h1>Now</h1>
-		<br />
+<div id="p404">
+	<h1>Now</h1>
+	<br />
 
-		<p class="pt-16">
-			I thought this would look mindful and reflective. (It's also easier to
-			track for things still in progress).
-		</p>
-	</div>
-</Layout>
+	<p class="pt-16">
+		I thought this would look mindful and reflective. (It's also easier to track
+		for things still in progress).
+	</p>
+</div>
 
 <style>
 	div#p404 {
