72e465c93ddeda03a7535b2b778a541e5164ee7a
ibro <sameoldlab@proton.me>
2024-08-28 20:44:38 +0100

    remove unused code

--- src/routes/note/[...slug]/+page.svelte
+++ src/routes/note/[...slug]/+page.svelte
@@ -3,7 +3,6 @@
 	let { data } = $props()
 
 	const { content: Content, entry, related } = $derived(data)
-	export const title = 'hello'
 </script>
 
 <Article {entry} modified={new Date(data.modified)} {related}>
